725afed38f7912b9cfc64cfc8cb8999a2ea2a152
Adam Brzozowski <adam-brzozowski@asseco.pl>
2019-09-22 21:48:16 +0200

    Added MongoDbRunner entrypoint

---
+++ src/MongoInMemory/MongoDbRunner.cs
@@ -0,0 +1,29 @@
+﻿using MongoInMemory.ConnectionMonitor;
+using MongoInMemory.MongoDbServerProcess;
+using MongoInMemory.ProcessExecutor;
+
+namespace MongoInMemory
+{
+    public static class MongoDbRunner
+    {
+        public static MongoDbServerDescriptor CreateMongoDbServerInstance()
+        {
+            var processExecutorFactory = new ProcessExecutorFactory();
+            var mongoDbServerProcessFactory = new MongoDbServerProcessFactory();
+            var mongoDbConnectionMonitorFactory = new MongoDbConnectionMonitorFactory();
+
+            var processExecutor = processExecutorFactory.CreateProcessExecutor();
+            var mongoDbServerProcess = mongoDbServerProcessFactory.CreateMongoDbServerProcess();
+            processExecutor.StartProcess(mongoDbServerProcess);
+
+            var mongoDbConnectionMonitor = mongoDbConnectionMonitorFactory.CreateMongoDbConnectionMonitor();
+            mongoDbConnectionMonitor.WaitForMongoDbServer(mongoDbServerProcess.ServerHost, mongoDbServerProcess.ServerPort);
+
+            return new MongoDbServerDescriptor()
+            {
+                Host = mongoDbServerProcess.ServerHost,
+                Port = mongoDbServerProcess.ServerPort
+            };
+        }
+    }
+}

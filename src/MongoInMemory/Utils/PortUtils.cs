85ae2de7e96157e114c5951257790140621382a0
Adam Brzozowski <adam-brzozowski@asseco.pl>
2019-09-22 20:36:59 +0200

    Added port utils

---
+++ src/MongoInMemory/Utils/PortUtils.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace MongoInMemory.Utils
+{
+    public static class PortUtils
+    {
+        public static int GetFreePortNumber()
+        {
+            var listener = new TcpListener(IPAddress.Loopback, 0);
+            listener.Start();
+            int resultPort = ((IPEndPoint)listener.LocalEndpoint).Port;
+            listener.Stop();
+            return resultPort;
+        }
+    }
+}

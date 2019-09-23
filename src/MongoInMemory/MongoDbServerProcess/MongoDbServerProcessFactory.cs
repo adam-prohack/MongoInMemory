ca324dcab61a394499e3b5277988f25c7a6c0fc4
Adam Brzozowski <adam-brzozowski@asseco.pl>
2019-09-23 18:01:05 +0200

    Fixed problem with mongod path

--- src/MongoInMemory/MongoDbServerProcess/MongoDbServerProcessFactory.cs
+++ src/MongoInMemory/MongoDbServerProcess/MongoDbServerProcessFactory.cs
@@ -1,6 +1,10 @@
 ﻿using MongoInMemory.Utils;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace MongoInMemory.MongoDbServerProcess
 {
@@ -15,14 +19,38 @@ namespace MongoInMemory.MongoDbServerProcess
         }
         private string getMongoDbDeamonPath()
         {
-            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
-                return Path.Combine(Directory.GetCurrentDirectory(), "resources", "win32-x86_64-2012plus", "mongod.exe");
-            else if (Environment.OSVersion.Platform == PlatformID.Unix && Environment.OSVersion.VersionString.ToLower().Contains("ubuntu"))
-                return Path.Combine(Directory.GetCurrentDirectory(), "resources", "linux-x86_64-ubuntu1804", "mongod");
-            else if (Environment.OSVersion.Platform == PlatformID.Unix && Environment.OSVersion.VersionString.ToLower().Contains("osx"))
-                return Path.Combine(Directory.GetCurrentDirectory(), "resources", "macos-x86_64", "mongod");
-            else
-                throw new NotSupportedException($"Current operating system is not supported");
+            var basePathes = new List<string>()
+            {
+                Directory.GetCurrentDirectory(),
+                Path.GetDirectoryName(Assembly.GetAssembly(typeof(MongoDbServerProcessFactory)).Location),
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                Path.GetDirectoryName(Assembly.GetEntryAssembly().Location),
+                Path.GetDirectoryName(Assembly.GetCallingAssembly().Location)
+            };
+            foreach (var basePath in basePathes.Distinct())
+            {
+                var resultPath = "";
+                if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+                    resultPath = Path.Combine(basePath, "resources", "mongodb-win32-x86_64-2012plus", "mongod.exe");
+                else if (Environment.OSVersion.Platform == PlatformID.Unix && Environment.OSVersion.VersionString.ToLower().Contains("ubuntu"))
+                    resultPath = Path.Combine(basePath, "resources", "mongodb-linux-x86_64-ubuntu1804", "mongod");
+                else if (Environment.OSVersion.Platform == PlatformID.Unix && Environment.OSVersion.VersionString.ToLower().Contains("osx"))
+                    resultPath = Path.Combine(basePath, "resources", "mongodb-macos-x86_64", "mongod");
+                else
+                    throw new NotSupportedException($"Current operating system is not supported");
+                Debug.WriteLine(resultPath);
+                if (File.Exists(resultPath)) { return resultPath; }
+            }
+            throw new FileNotFoundException($"Couldn't find mongod file");
+
+            //if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            //    return Path.Combine(libAssemblyLocation, "resources", "win32-x86_64-2012plus", "mongod.exe");
+            //else if (Environment.OSVersion.Platform == PlatformID.Unix && Environment.OSVersion.VersionString.ToLower().Contains("ubuntu"))
+            //    return Path.Combine(libAssemblyLocation, "resources", "linux-x86_64-ubuntu1804", "mongod");
+            //else if (Environment.OSVersion.Platform == PlatformID.Unix && Environment.OSVersion.VersionString.ToLower().Contains("osx"))
+            //    return Path.Combine(libAssemblyLocation, "resources", "macos-x86_64", "mongod");
+            //else
+            //    throw new NotSupportedException($"Current operating system is not supported");
         }
         private string getMongoDbServerArguments(int port, string ipAddress, string cwd)
         {
